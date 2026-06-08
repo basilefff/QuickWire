4d1130e19161bb225c22279c568a2e7623cab918
Чай Хуа Пу <basilefff@gmail.com>
2026-06-08 20:48:17 +0400

    feat: added debugging to the project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +11,8 @@ int main(int argc, char *argv[])
 					 &app, []() { QCoreApplication::exit(-1); },
 	Qt::QueuedConnection);
 	engine.loadFromModule("QuickWire", "Main");
+
+	qDebug() << "Let's test the debug tools\n";
 
 	return app.exec();
 }
