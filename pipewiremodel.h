90ec6eeeead91962ee0e1d1a4ed0634c68959416
Чай Хуа Пу <basilefff@gmail.com>
2026-06-08 20:56:03 +0400

    feat: added empty PipeWire model class

---
+++ pipewiremodel.h
@@ -0,0 +1,21 @@
+#ifndef PIPEWIREMODEL_H
+#define PIPEWIREMODEL_H
+
+#include <QAbstractListModel>
+
+class PipeWireModel : public QAbstractListModel
+{
+	Q_OBJECT
+
+public:
+	explicit PipeWireModel(QObject *parent = nullptr);
+
+	// Basic functionality:
+	int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private:
+};
+
+#endif // PIPEWIREMODEL_H
